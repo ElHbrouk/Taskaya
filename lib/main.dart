a11494fac184b80532720a9b36465149a6acd06b
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-25 19:31:57 +0300

    Refactor main.dart to use MaterialApp.router and initialize shared preferences
    - Removed individual imports for Hive and specific view files, replacing them with a single import for hive_flutter and core utilities.
    - Initialized shared preferences using `SharedPreferencesSingleton.init()` in the `main` function.
    - Replaced `MaterialApp` with `MaterialApp.router` to enable routing configuration.
    - Updated the app's routing configuration to use `AppRoutes.router`.
    - Removed commented-out code related to Hive adapter registration and box opening.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:taskaya/constats.dart';
-import 'package:taskaya/features/home/data/models/note_model.dart';
-import 'package:taskaya/features/home/presentation/views/home_view.dart';
-import 'package:taskaya/features/splash/presentation/views/splash_view.dart';
+import 'package:taskaya/core/services/shared_preferences_singleton.dart';
+import 'package:taskaya/core/utils/app_routes.dart';
 
 void main() async {
   await Hive.initFlutter();
+  SharedPreferencesSingleton.init();
   // Hive.registerAdapter(NoteModelAdapter());
   // var box = await Hive.openBox<NoteModel>(noteBox);
   runApp(const MyApp());
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Taskaya',
       theme: ThemeData(
@@ -26,7 +24,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const SplashView(),
+      routerConfig: AppRoutes.router,
     );
   }
 }
