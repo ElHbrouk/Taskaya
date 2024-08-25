7809dab1aeab9310a9e08c5d1efa0471e68cf141
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-25 19:30:51 +0300

    Add CustomGradientWidget for consistent UI styling
    This commit introduces a new `CustomGradientWidget` to the project, enhancing UI consistency by providing a reusable widget for applying gradient backgrounds. The widget is designed to wrap around any child widget, allowing for flexible use across different parts of the application. It supports optional margin customization to adjust spacing as needed.
    
    The `CustomGradientWidget` utilizes a linear gradient with predefined colors from `AppColors`, ensuring a unified visual style. This addition simplifies the implementation of gradient backgrounds, reducing redundancy and promoting cleaner code.

--- lib/core/widgets/custom_gradient_widget.dart
+++ lib/core/widgets/custom_gradient_widget.dart
@@ -2,20 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:taskaya/core/utils/app_colors.dart';
 
 class CustomGradientWidget extends StatelessWidget {
-  const CustomGradientWidget({super.key, required this.child});
+  const CustomGradientWidget({
+    super.key,
+    required this.child,
+    this.margin,
+  });
   final Widget child;
+  final EdgeInsetsGeometry? margin;
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: margin,
       width: double.infinity,
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
-            AppColors.primaryColor1,
-            AppColors.primaryColor2,
-            AppColors.primaryColor3,
+            AppColors.linearColor1,
+            AppColors.linearColor2,
+            AppColors.linearColor3,
           ],
         ),
       ),
