0ef09b6cd3c5dab473d14f2a480a3380f1128e55
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-25 19:31:05 +0300

    Add new text styles and update existing ones in app_fonts.dart
    This commit introduces new text styles for the application, enhancing the typography options available. Specifically, it adds bold58, bold24, bold16, bold12, semiBold16, semiBold12, regular20, regular16, regular14, and regular12 text styles. Each style is designed to be responsive, adjusting based on the context in which it is used. Additionally, it updates the color references in existing styles to align with the latest design guidelines, ensuring consistency across the application.

--- lib/core/utils/app_fonts.dart
+++ lib/core/utils/app_fonts.dart
@@ -5,6 +5,24 @@ import 'package:taskaya/core/utils/app_colors.dart';
 class AppFonts {
   static const String fontFamily = 'Alexandria';
 
+  static TextStyle bold58(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.bold,
+      fontSize: getResponsiveFont(context, fontSize: 58),
+      color: Colors.white,
+    );
+  }
+
+  static TextStyle bold24(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.bold,
+      fontSize: getResponsiveFont(context, fontSize: 24),
+      color: AppColors.secondary,
+    );
+  }
+
   static TextStyle bold18(context) {
     return TextStyle(
       fontFamily: fontFamily,
@@ -14,12 +32,75 @@ class AppFonts {
     );
   }
 
-  static TextStyle boldSemi16(context) {
+  static TextStyle bold16(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.bold,
+      fontSize: getResponsiveFont(context, fontSize: 16),
+      color: Colors.white,
+    );
+  }
+
+  static TextStyle bold12(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.bold,
+      fontSize: getResponsiveFont(context, fontSize: 12),
+      color: Colors.white,
+    );
+  }
+
+  static TextStyle semiBold16(context) {
     return TextStyle(
       fontFamily: fontFamily,
       fontWeight: FontWeight.w600,
       fontSize: getResponsiveFont(context, fontSize: 16),
-      color: AppColors.primaryColor2,
+      color: AppColors.linearColor2,
+    );
+  }
+
+  static TextStyle semiBold12(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w600,
+      fontSize: getResponsiveFont(context, fontSize: 12),
+      color: Colors.white,
+    );
+  }
+
+  static TextStyle regular20(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w400,
+      fontSize: getResponsiveFont(context, fontSize: 20),
+      color: AppColors.primary,
+    );
+  }
+
+  static TextStyle regular16(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w400,
+      fontSize: getResponsiveFont(context, fontSize: 16),
+      color: AppColors.linearColor2,
+    );
+  }
+
+  static TextStyle regular14(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w400,
+      fontSize: getResponsiveFont(context, fontSize: 14),
+      color: AppColors.secondary,
+    );
+  }
+
+  static TextStyle regular12(context) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w400,
+      fontSize: getResponsiveFont(context, fontSize: 12),
+      color: AppColors.linearColor2,
     );
   }
 }
