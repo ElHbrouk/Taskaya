6d2b9361d3dc1b4a33b49992d8b7be3c354e4bda
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-25 19:29:50 +0300

    Refactor color constants in AppColors class
    Rename and add color constants for better clarity and consistency. Renamed primaryColor1, primaryColor2, and primaryColor3 to linearColor1, linearColor2, and linearColor3 respectively. Introduced new color constants primary, secondary, and blueAccent to enhance the semantic meaning of color usage within the application. This change aims to improve readability and maintainability of the codebase by providing more descriptive color names.

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor1 = Color(0xFF222831);
-  static const Color primaryColor2 = Color(0xFF393E46);
-  static const Color primaryColor3 = Color(0xFF2A2A2E);
+  static const Color linearColor1 = Color(0xFF222831);
+  static const Color linearColor2 = Color(0xFF393E46);
+  static const Color linearColor3 = Color(0xFF2A2A2E);
+  static const Color primary = Color(0xFFEEEEEE);
+  static const Color secondary = Color(0xFF222831);
+  static const Color blueAccent = Color(0xFF00ADB5);
 }
